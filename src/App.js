4708eee1314abf4c01f7ab708b75bc423e846678
Vijay Rangan <vijay.rangan@gmail.com>
2019-01-03 13:24:31 +0530

    Remove scaffolded code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class App extends Component {
   render() {
-    return (
-      <div>
-      </div>
-    );
+    return <div />;
   }
 }
 
